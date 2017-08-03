c22f2a8016730cc26ce10ed3a1a4bb2fb59a12a3
yz <yzgg1020@gmail.com>
2017-08-03 10:01:03 +0000

    update

--- Cafe.scala
+++ Cafe.scala
@@ -3,9 +3,20 @@ class Cafe {
 	
 	def buyCoffee(cc: CreditCard): Coffee = {
 		val cup = new Coffee()
-		// 副作用， 信用卡计费
+		// 副作用， 信用卡计费细节
+		// 额外发生的行为
 		cc.charge(cup.price)
 
 		cup
 	}
+	
+	// 忽略支付细节，更模块化，便于测试；未完全消除副作用
+	def buyCoffee(cc: CreditCard, p: Payments): Coffee = {
+		val cup = new Coffee()
+		p.charge(cc, cup.price)
+		
+		cup
+	}
+	
+
 }
