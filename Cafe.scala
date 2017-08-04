bcc4234b928665e0cf571cd7152b5adb1f938c8d
yz <yzgg1020@gmail.com>
2017-08-04 01:48:07 +0000

    update fp

--- Cafe.scala
+++ Cafe.scala
@@ -18,5 +18,23 @@ class Cafe {
 		cup
 	}
 	
-
+	// 函数式，去除副作用
+	def buyCoffee(cc: CreditCard): (Coffee, Charge) = {
+		val cup = new Coffee()
+		(cup, Charge(cc, cup.price))
+	}
+	
+	case class Charge(cc: CreditCard, amount: Double) {
+		def combine(other: Charge): Charge = 
+			if (cc = other.cc)
+				Charge(cc, amount + other.amount)
+			else
+				throw new Exception("can't combine charges to different cards")
+	}
+	
+	def buyCoffees(cc: CreditCard, n: Int): (List[Coffee], Charge) = {
+		val purchases: List[(Coffee, Charge)] = List.fill(n)(buyCoffee(cc))
+		val (coffees, charges) = purchases.unzip
+		(coffees, charge.reduce((c1, c2) => c1.combine(c2)))
+	}
 }
