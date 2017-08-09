8af6cbec4afd5fb078cb91e051796cee7b03490c
yz <yzgg1020@gmail.com>
2017-08-09 09:40:57 +0000

    hof p2.2

--- c2/hof.scala
+++ c2/hof.scala
@@ -10,16 +10,63 @@ object hof {
         go(n, 1)
     }
     
+    // http://cjwebb.github.io/blog/2013/10/30/fibonacci-numbers-in-scala/
+    
     def fib(n: Int): Int = {
         def loop(n: Int, pre: Int, cur: Int): Int = {
-            if (n < 2) pre
-            else loop(n - 1, cur, pre + cur)
+            
+            // if (n < 2) pre
+            // else loop(n - 1, cur, pre + cur)
+            
+            n match {
+                case 0 => pre
+                case _ => loop(n - 1, cur, pre + cur)
+            }
         }
         
         loop(n, 0, 1)
     }
     
+    // hof
+    def formatResult(name: String, n: Int, f: Int => Int): String = {
+        "the %s of %d is %d".format(name, n, f(n))
+    }
+ 
+    // def findFirst(ss: Array[String], key: String): Int = {
+    //     def loop(n: Int): Int = 
+    //         if (n >= ss.length) -1
+    //         else if (ss(n) == key) n
+    //         else loop(n + 1)
+            
+    //     loop(0)
+    // }
+        
+    // 泛型函数
+    def findFirst[A](ss: Array[A], p: A => Boolean): Int = {
+        def loop(n: Int): Int = 
+            if (n >= ss.length) -1
+            else if (p(ss(n))) n
+            else loop(n + 1)
+            
+        loop(0)
+    }
+    
+    // p2.2
+    def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+        def loop(n: Int): Boolean = 
+            if (n >= as.length - 1) true
+            else if (ordered(as(n), as(n + 1))) loop(n+1)
+            else false
+            
+        loop(0)
+    }
+    
+    
     def main(args: Array[String]) {
-        print(fib(5))
+        println(formatResult("factorial", 5, factorial))
+        println(formatResult("fibonacci", 5, fib))
+        
+        println(isSorted(Array(1, 9, 5, 7), (x: Int, y: Int) => x < y))
+        println(isSorted(Array(1, 3, 5, 7), (x: Int, y: Int) => x < y))
     }
 }
