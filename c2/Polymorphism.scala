3787f0cd0e14194374527ae00713b70779d0484b
yz <yzgg1020@gmail.com>
2017-08-10 06:03:36 +0000

    finish polymorphism

--- c2/Polymorphism.scala
+++ c2/Polymorphism.scala
@@ -2,10 +2,19 @@ object Polymorphism {
     
     def curry[A,B,C](f: (A, B) => C): A => (B => C) = {
         // (a: A) => (b => f(a, b))
+        // => 右箭头为 右结合 
         a => b => f(a, b)
     }
  
     def uncurry[A,B,C](f: A => B => C): (A, B) => C = {
         (a, b) => f(a)(b)
     }
+    
+    def compose[A,B,C](f: B => C, g: A => B): A => C = {
+        a => f(g(a))
+    }
+    
+    // Function1 的 compose 或 andThen
+    // val f = (x: Double) => math.Pi / 2-x
+    // val cos = f andThen math.sin
 }
