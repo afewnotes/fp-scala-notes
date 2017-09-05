b49bea69164a3b645c24630cefb96c76d08e7e6c
yz <yzgg1020@gmail.com>
2017-09-05 08:52:23 +0000

    4.exception.option

--- c4-exception/sample.scala
+++ c4-exception/sample.scala
@@ -1,5 +1,9 @@
 object sample {
     
+    def main(args: Array[String]){
+        println(failingFn(10))
+    }
+    
     def failingFn(i: Int): Int = {
         // 不是引用透明 （表达式可以被它引用的值替代，不依赖上下文，可本地推导）
         // val y: Int = throw new Exception("fail") 
@@ -13,8 +17,32 @@ object sample {
         }
     }
     
-    def main(args: Array[String]){
-        println(failingFn(10))
-    }
+    // 部分函数：对一些输入没有结果
+    // 缺点：
+    //  - 允许错误无声的传播，调用者可能忘了检查，导致后续代码不能正常工作;
+    //  - 显式 if 检测数据正确性
+    //  - 不适用于多态
+    //  - 需要调用者做一些事情
+    def mean(xs: Seq[Double]): Double = 
+        if (xs.isEmpty)
+            throw new ArithmeticException("mean of empty list")
+        else xs.sum / xs.length
+        
+    // 完全函数
+    // 缺点：需要调用者知道如何处理未定义的情况
+    def mean_1(xs: IndexedSeq[Double], onEmpty: Double): Double = 
+        if (xs.isEmpty) onEmpty
+        else xs.sum / xs.length
+        
+    // Option：结果不一定总被定义
+    sealed trait Option[+A]
+    // 对应已被定义的情况
+    case class Some[+A](get: A) extends Option[A]
+    // 对应未被定义的情况
+    case object None extends Option[Nothing]
     
+    // 完全函数：每个输入值都有对应输出类型的值
+    def mean_2(xs: Seq[Double]): Option[Double] = 
+        if (xs.isEmpty) None
+        else Some(xs.sum / xs.length)
 }
