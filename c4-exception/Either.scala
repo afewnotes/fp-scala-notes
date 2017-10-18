4be167c90eef1d84705f8a35dffa1d4f17bcf945
yz <yzgg1020@gmail.com>
2017-10-18 02:22:41 +0000

    exercise 4.7

--- c4-exception/Either.scala
+++ c4-exception/Either.scala
@@ -56,5 +56,23 @@ object Either {
             tickets <- Try { numberOfSpeedingTickets.toInt }
         }
     
-    
+    // exercise 4.7
+    // 实现 sequence, traverse，遇到错误返回第一个错误
+    // def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = 
+    //     es match {
+    //         case Nil => Right(Nil)
+    //         case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
+    //     }
+        
+    def traverse[E, A, B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = 
+        es match {
+            case Nil => Right(Nil)
+            case h :: t => (f(h) map2 traverse(t)(f))(_ :: _)
+        }
+        
+    def traverse_1[E, A, B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = 
+        es.foldRight[Either[E, List[B]]](Right(Nil))((a,b) => f(a).map2(b)(_ :: _))
+        
+    def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = 
+        traverse(es)(x => x)
 }
