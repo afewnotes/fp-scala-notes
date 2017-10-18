f7ab1716a3b322c639a14e221b4b7fd3e0ff08dd
yz <yzgg1020@gmail.com>
2017-10-18 05:57:58 +0000

    finish either

--- c4-exception/Either.scala
+++ c4-exception/Either.scala
@@ -75,4 +75,19 @@ object Either {
         
     def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = 
         traverse(es)(x => x)
+        
+    case class Person(name: Name, age: Age)
+    sealed class Name(val value: String)
+    sealed class Age(val value: Int)
+    
+    def mkName(name: String): Either[String, Name] = 
+        if (name == "" || name == null) Left("Name is empty")
+        else Right(new Name(name))
+        
+    def mkAge(age: Int): Either[String, Age] = 
+        if (age < 0) Left("Age is out of range")
+        else Right(new Age(age))
+        
+    def mkPerson(name: String, age: Int): Either[String, Person] = 
+        mkName(name).map2(mkAge(age))(Person(_, _))
 }
