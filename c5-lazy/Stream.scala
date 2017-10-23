6d6b396fc777438c8f8610b57af583c90899c91f
yz <yzgg1020@gmail.com>
2017-10-23 08:26:36 +0000

    exercise

--- c5-lazy/Stream.scala
+++ c5-lazy/Stream.scala
@@ -109,4 +109,26 @@ sealed trait Stream[+A] {
             if (p(h)) cons(h, t)
             else empty
         )
+        
+    // exercise 5.6
+    def headOptionViaFoldRight: Option[A] = 
+        foldRight(None: Option[A])((a, _) => Some(a))
+        
+    // exercise 5.7
+    def map[B](f: A => B): Stream[B] =
+        foldRight(empty[B])((h, t) => cons(f(h), t))
+        
+    def filter(f: A => Boolean): Stream[A] =
+        foldRight(empty[A])((h, t) =>
+            if (f(h)) cons(h(), t)
+            else t
+        )
+        
+    def append[B >: A](s: => Stream[B]): Stream[B] =
+        foldRight(s)((h,t) => cons(h, t))
+        
+    def flatMap[B](f: A => Stream[B]): Stream[B] =
+        foldRight(empty[B])((h, t) => f(h) append t)
+        
+    
 }
