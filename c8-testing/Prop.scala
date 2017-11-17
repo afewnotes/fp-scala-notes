2f38b10c31f80fa61d555ba4ab1147e616a8578c
yz <yzgg1020@gmail.com>
2017-11-17 07:13:42 +0000

    trait and api

--- c8-testing/Prop.scala
+++ c8-testing/Prop.scala
@@ -1,5 +1,6 @@
 
 trait Prop {
+    // 简单的谓词
     def check: Boolean
 }
 
@@ -10,4 +11,10 @@ def &&(p: Prop): Prop = new Prop {
 
 object Prop {
     type SuccessCount = Int
+    type FailedCase = String // 不关心失败的类型，返回失败描述详情即可
+}
+
+trait Prop {
+    // 失败的场景下返回 Left 即可知道失败原因，失败前有多少次成功
+    def check: Either[(FailedCase, SuccessCount), SuccessCount]
 }
